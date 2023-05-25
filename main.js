97722258a411575c063fbb4ed6eb04d977c62431
inacio4234 <121254172+inacio4234@users.noreply.github.com>
2023-05-25 16:05:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var nome="Inácio ";
+console.log(nome);
+
+var sobrenome="Lopes";
+console.log(sobrenome);
+
+var nomecompleto=nome.concat(sobrenome);
+console.log(nomecompleto);
+
+var b=32;
+
+var a=45;
+console.log(a+b);
